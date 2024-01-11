6711bb95c38854d0b0a6a667a5846029e42cc875
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-11 19:07:57 +0530

    change layout of header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import logo from "../assets/memory_game_logo.jfif"
-import '../css/header.css'
-export default function Header({points,round,maxPoints}) {
-
-    return <>
-        <header>
-            <div className="logo"><img src={logo} alt="logo" /></div>
-            <div>Memory Game</div>
-            <div className="score">
-                <div className="score-current">
-                    <p>Points</p>{points}</div>
-                <div className="score-highest">
-                    <p>Max Points</p>{maxPoints}</div>
-                <div><p>Round No</p>{round}</div>
-            </div>
-        </header>
-        
-    </>;
+import logo from "../assets/memory_game_logo.jfif";
+import "../css/header.css";
+export default function Header({ points, round, maxPoints }) {
+    return (
+        <>
+            <header>
+                <div className="logo">Memory Game</div>
+                <div className="score">
+                    <div className="score-current">
+                        <p>Current Points</p>
+                        <p>{points}</p>
+                    </div>
+                    <div className="score-highest">
+                        <p>Max Points</p>
+                        <p>{maxPoints}</p>
+                    </div>
+                    <div>
+                        <p>Round No</p>
+                        <p>{round}</p>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 }
