09267b48ce27b0c9dfaaab22b26277b078271b83
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2024-01-11 19:31:28 +0530

    remove background of card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,8 +10,13 @@ function App() {
     const [maxPoints, setMaxPoints] = useState(0);
     return (
         <>
-            <Header {...{points,round,maxPoints}} />
-            <Body points={points} setPoints={setPoints} setMaxPoints={setMaxPoints}  setRound={setRound}/>
+            <Header {...{ points, round, maxPoints }} />
+            <Body
+                points={points}
+                setPoints={setPoints}
+                setMaxPoints={setMaxPoints}
+                setRound={setRound}
+            />
             <Footer />
         </>
     );
